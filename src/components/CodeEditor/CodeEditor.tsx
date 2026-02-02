86b0a7d06d9e1ceb11d8b6db8da93359bcdb0556
ophirgal <ophirgal2@gmail.com>
2026-02-02 18:09:22 +0200

    Makd the output console shared

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -30,12 +30,20 @@ export default function CodeEditor() {
     const [fontSize, setFontSize] = useState(14);
     const editorRef = useRef<monaco.IStandaloneCodeEditor | null>(null);
     const bindingRef = useRef<MonacoBinding | null>(null);
+    const outputContainerRef = useRef<HTMLDivElement>(null);
     // const cursorWidgetRef = useRef<CursorWidget | null>(null);
     const { yDoc, currentLanguage, setCurrentLanguage,
         remoteProfile /*, localProfile, updateLocalProfile */ } = useCollaboration();
     const { isDarkMode } = useTheme();
 
     const runtime = useMemo<RuntimeEngine>(() => runtimeRegistry[currentLanguage], [currentLanguage]);
+
+    function scrollOutputContainerToBottom() {
+        if (outputContainerRef.current) {
+            const div = outputContainerRef.current.childNodes[0] as HTMLDivElement
+            div.scrollTop = div.scrollHeight;
+        }
+    }
 
     const handleIncreaseFontSize = () => {
         setFontSize((prevFontSize) => prevFontSize + 1);
@@ -55,12 +63,9 @@ export default function CodeEditor() {
             const code = editorRef.current?.getValue() || "";
             await runtime.runCode(code);
         } catch (err) {
-            setOutput((prev) => prev + (err as Error).toString());
-        }
-
-        const outputDiv = document.getElementById('output-panel')?.childNodes[0] as HTMLDivElement;
-        if (outputDiv) {
-            outputDiv.scrollTop = outputDiv.scrollHeight;
+            const sharedOutputId = `${runtime.languageId}-output`;
+            const yText = yDoc.getText(sharedOutputId);
+            yText.insert(yText.toString().length, (err as Error).toString());
         }
     }, [runtime]);
 
@@ -76,8 +81,12 @@ export default function CodeEditor() {
         //     updateLocalProfile({ editors: { ...localProfile.editors, [runtime.languageId]: e.position } });
         // });
 
-        // Destroy old binding if it exists
-        bindingRef.current?.destroy();
+        // Destroy old binding if it exists 
+        // TODO: this does not seem to be needed since we have it on cleanup. remove soon if no need for it.
+        // if (bindingRef.current) {
+        //     bindingRef.current.destroy();
+        //     bindingRef.current = null;
+        // }
 
         // Rebind MonacoBinding when yText changes (language switch)
         bindingRef.current = new MonacoBinding(
@@ -92,9 +101,11 @@ export default function CodeEditor() {
         };
     }, [runtime]);
 
-    const handleClearOutput = () => {
-        setOutput("");
-    };
+    const handleClearOutput = useCallback(() => {
+        const sharedOutputId = `${runtime.languageId}-output`;
+        const yText = yDoc.getText(sharedOutputId);
+        yText.delete(0, yText.toString().length);
+    }, [runtime]);
 
     // Respond to remote cursor position changes 
     useEffect(() => {
@@ -127,11 +138,14 @@ export default function CodeEditor() {
 
         let disposed = false;
 
+        const sharedOutputId = `${runtime.languageId}-output`;
+        const yText = yDoc.getText(sharedOutputId);
+
         const loadRuntime = async () => {
             try {
-                await runtime.load((output) => {
+                await runtime.load((newOutput) => {
                     if (!disposed) {
-                        setOutput((prev) => prev + output);
+                        yText.insert(yText.toString().length, newOutput);
                     }
                 });
                 if (!disposed) {
@@ -145,13 +159,39 @@ export default function CodeEditor() {
         };
 
         loadRuntime();
-        setOutput(""); // Reset output when runtime changes
+        setOutput(yText.toString());
 
         return () => {
             disposed = true;
             runtime.dispose();
         };
     }, [runtime]);
+
+    // Scroll to the bottom of the output container when output changes
+    useEffect(() => {
+        scrollOutputContainerToBottom();
+    }, [output])
+
+    useEffect(() => {
+        const observerRemovers: (() => void)[] = []
+
+        Object.keys(runtimeRegistry).forEach(languageId => {
+            const sharedOutputId = `${languageId}-output`;
+            const yText = yDoc.getText(sharedOutputId);
+
+            // observe yText updates (both local and remote updates) and update local output
+            const handler = () => {
+                if (currentLanguage === languageId) {
+                    setOutput(yText.toString())
+                }
+            }
+
+            yText.observe(handler);
+            observerRemovers.push(() => yText.unobserve(handler))
+        });
+
+        return () => observerRemovers.forEach(fn => { try { fn() } catch { } });
+    }, [currentLanguage]);
 
     return (
         <div className="flex flex-col h-full gap-2">
@@ -207,7 +247,7 @@ export default function CodeEditor() {
                     <ResizableHandle withHandle
                         className="flex justify-center items-center w-full h-[15px] bg-transparent hover:bg-indigo-100 dark:hover:bg-indigo-900"
                         customHandle={<GripHorizontal className="size-2.5" />} />
-                    <ResizablePanel id="output-panel" className="relative border p-2 pb-2 bg-gray-50 dark:bg-neutral-800 text-gray-500 dark:text-gray-400 overflow-y-scroll" defaultSize={35}>
+                    <ResizablePanel elementRef={outputContainerRef} className="relative border p-2 pb-2 bg-gray-50 dark:bg-neutral-800 text-gray-500 dark:text-gray-400 overflow-y-scroll" defaultSize={35}>
                         {output &&
                             <div className="sticky top-0 z-10 h-0 flex justify-end">
                                 <Button disabled={!output} variant="ghost" size="sm" onClick={handleClearOutput} title="Clear output"
