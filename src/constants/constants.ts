41c397b0c814ef162ec1e898eae48b4d18d1c207
ophirgal <ophirgal2@gmail.com>
2026-02-03 18:28:03 +0200

    fix nav links (didnt work in rooms page)

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,8 +1,8 @@
 const navLinks = [
     { href: "/", label: "Home" },
-    { href: "#pricing", label: "Pricing" },
-    { href: "#faq", label: "FAQ" },
-    { href: "#contact", label: "Contact" },
+    { href: "/#pricing", label: "Pricing" },
+    { href: "/#faq", label: "FAQ" },
+    { href: "/#contact", label: "Contact" },
 ]
 
 const localVideoElementId = "local-video"
