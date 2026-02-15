eb79c80ca1c64f46775b368585909c9e2293cd81
ophirgal <ophirgal2@gmail.com>
2026-02-15 13:37:29 +0200

    adapt home page to show static demo image on Safari instead of demo video

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,6 +8,7 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion"
 
+const isProbablySafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
 const demoVideo = "/assets/videos/demo_video.webm"
 const demoImage = "/assets/demo_image.png"
 
@@ -138,7 +139,10 @@ function HeroSection() {
                 [synk] Up!
             </a>
         </div>
-        <video src={demoVideo} className="hidden lg:block flex-1 w-100 scale-150 md:pr-25 md:pl-5" autoPlay loop muted playsInline />
+        {isProbablySafari ? 
+         <img src={demoImage} className="hidden lg:block flex-1 w-100 scale-150 md:pr-25 md:pl-5" />
+         : <video src={demoVideo} className="hidden lg:block flex-1 w-100 scale-150 md:pr-25 md:pl-5" autoPlay loop muted playsInline />}
+        
     </section>
 
 }
